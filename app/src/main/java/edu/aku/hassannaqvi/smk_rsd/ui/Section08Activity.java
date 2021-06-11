12a3ff6aa284b7521da4c79be03e34080ec2da7b
SAJID LATIF <indus1530@gmail.com>
2021-06-11 12:51:05 +0500

    activity_section08.xml import

---
+++ app/src/main/java/edu/aku/hassannaqvi/smk_rsd/ui/Section08Activity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.smk_rsd.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.smk_rsd.R;
+
+public class Section08Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section08);
+    }
+}
