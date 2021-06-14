d08679c36bc28766a71eca5d2597823f0be14ce5
SAJID LATIF <indus1530@gmail.com>
2021-06-13 18:10:01 -1200

    Section06Activity funDef

--- app/src/main/java/edu/aku/hassannaqvi/smk_rsd/ui/Section06Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/smk_rsd/ui/Section06Activity.java
@@ -1,6 +1,8 @@
 package edu.aku.hassannaqvi.smk_rsd.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,15 +14,43 @@ public class Section06Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section06);
+        setupSkips();
     }
 
 
-    void setupSkips() {
+    private void setupSkips() {
+
+    }
+
+
+    private void saveDraft() {
 
     }
 
 
     private boolean updateDB() {
+        return true;
+    }
+
+
+    public void BtnContinue(View view) {
+        if (!formValidation()) return;
+        saveDraft();
+        if (updateDB()) {
+            Toast.makeText(this, "Patient Added", Toast.LENGTH_SHORT).show();
+            finish();
+            //gotoActivityWithPutExtra(this, SectionMobileHealth.class, "complete", true);
+        }
+    }
+
+
+    public void BtnEnd(View view) {
+        //AppUtilsKt.contextEndActivity(this);
+    }
+
+
+    private boolean formValidation() {
+        /*return Validator.emptyCheckingContainer(this, bi.GrpName);*/
         return true;
     }
 
