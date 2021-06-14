c5333b44a823fdd9b6519bc737f0c395e928e28a
SAJID LATIF <indus1530@gmail.com>
2021-06-14 22:12:15 +1400

    someChanges funDef

--- app/src/main/java/edu/aku/hassannaqvi/smk_rsd/ui/Section06Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/smk_rsd/ui/Section06Activity.java
@@ -5,15 +5,21 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import edu.aku.hassannaqvi.smk_rsd.R;
+import edu.aku.hassannaqvi.smk_rsd.data.model.Form;
+import edu.aku.hassannaqvi.smk_rsd.databinding.ActivitySection06Binding;
+
+import static edu.aku.hassannaqvi.smk_rsd.core.MainApp.form;
 
 public class Section06Activity extends AppCompatActivity {
+    ActivitySection06Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section06);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section06);
         setupSkips();
     }
 
@@ -24,6 +30,9 @@ public class Section06Activity extends AppCompatActivity {
 
 
     private void saveDraft() {
+        form = new Form();
+        //form.se2001 = bi.fpr01.getText().toString().isEmpty ? "-1" : bi.fpr01.getText().toString();
+        form.se2001 = bi.fpr0197.isChecked() ? "97" : "-1";
 
     }
 
