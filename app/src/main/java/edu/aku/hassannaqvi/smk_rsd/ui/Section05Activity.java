9dd92e216771e7a60dd8f9a6067fa20b8f975a1c
ahs1n <ahsanpervez74@gmail.com>
2021-06-15 17:18:34 +0500

    Add Checkbox checked condition in Section05Activity

--- app/src/main/java/edu/aku/hassannaqvi/smk_rsd/ui/Section05Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/smk_rsd/ui/Section05Activity.java
@@ -24,6 +24,171 @@ public class Section05Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         bi = DataBindingUtil.setContentView(this, R.layout.activity_section05);
         setupSkips();
+        setListeners();
+    }
+
+    private void setListeners() {
+
+        bi.obs0197.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs01.setEnabled(false);
+                bi.obs01.setText(null);
+            } else
+                bi.obs01.setEnabled(true);
+        });
+
+        bi.obs0297.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs02.setEnabled(false);
+                bi.obs02.setText(null);
+            } else
+                bi.obs02.setEnabled(true);
+        });
+
+        bi.obs0397.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs03.setEnabled(false);
+                bi.obs03.setText(null);
+            } else
+                bi.obs03.setEnabled(true);
+        });
+
+        bi.obs0497.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs04.setEnabled(false);
+                bi.obs04.setText(null);
+            } else
+                bi.obs04.setEnabled(true);
+        });
+
+        bi.obs0597.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs05.setEnabled(false);
+                bi.obs05.setText(null);
+            } else
+                bi.obs05.setEnabled(true);
+        });
+
+        bi.obs0697.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs06.setEnabled(false);
+                bi.obs06.setText(null);
+            } else
+                bi.obs06.setEnabled(true);
+        });
+
+        bi.obs0797.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs07.setEnabled(false);
+                bi.obs07.setText(null);
+            } else
+                bi.obs07.setEnabled(true);
+        });
+
+        bi.obs0897.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs08.setEnabled(false);
+                bi.obs08.setText(null);
+            } else
+                bi.obs08.setEnabled(true);
+        });
+
+        bi.obs0997.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs09.setEnabled(false);
+                bi.obs09.setText(null);
+            } else
+                bi.obs09.setEnabled(true);
+        });
+
+        bi.obs1097.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs10.setEnabled(false);
+                bi.obs10.setText(null);
+            } else
+                bi.obs10.setEnabled(true);
+        });
+
+        bi.obs1197.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs11.setEnabled(false);
+                bi.obs11.setText(null);
+            } else
+                bi.obs11.setEnabled(true);
+        });
+
+        bi.obs1297.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs12.setEnabled(false);
+                bi.obs12.setText(null);
+            } else
+                bi.obs12.setEnabled(true);
+        });
+
+        bi.obs1397.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs13.setEnabled(false);
+                bi.obs13.setText(null);
+            } else
+                bi.obs13.setEnabled(true);
+        });
+
+        bi.obs1497.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs14.setEnabled(false);
+                bi.obs14.setText(null);
+            } else
+                bi.obs14.setEnabled(true);
+        });
+
+        bi.obs1597.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs15.setEnabled(false);
+                bi.obs15.setText(null);
+            } else
+                bi.obs15.setEnabled(true);
+        });
+
+        bi.obs1697.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs16.setEnabled(false);
+                bi.obs16.setText(null);
+            } else
+                bi.obs16.setEnabled(true);
+        });
+
+        bi.obs1797.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs17.setEnabled(false);
+                bi.obs17.setText(null);
+            } else
+                bi.obs17.setEnabled(true);
+        });
+
+        bi.obs1897.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs18.setEnabled(false);
+                bi.obs18.setText(null);
+            } else
+                bi.obs18.setEnabled(true);
+        });
+
+        bi.obs1997.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs19.setEnabled(false);
+                bi.obs19.setText(null);
+            } else
+                bi.obs19.setEnabled(true);
+        });
+
+        bi.obs2097.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                bi.obs20.setEnabled(false);
+                bi.obs20.setText(null);
+            } else
+                bi.obs20.setEnabled(true);
+        });
+
     }
 
 
