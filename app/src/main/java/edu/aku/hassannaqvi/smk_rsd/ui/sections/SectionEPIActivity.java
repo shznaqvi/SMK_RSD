aeb7b076ed62b8daaa3d980ce3f64c31a5d69b4a
shznaqvi <shznaqvi@gmail.com>
2021-06-25 09:05:43 +0500

    - EPI addform correction

--- app/src/main/java/edu/aku/hassannaqvi/smk_rsd/ui/sections/SectionEPIActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/smk_rsd/ui/sections/SectionEPIActivity.java
@@ -85,7 +85,7 @@ public class SectionEPIActivity extends AppCompatActivity {
     }
 
     private boolean addForm() {
-        //if (!form.get_ID().equals("")) return true;
+        if (!form.getId().equals("")) return true;
         DatabaseHelper db = appInfo.dbHelper;
         long rowid = db.addForm(form);
         form.setId(String.valueOf(rowid));
